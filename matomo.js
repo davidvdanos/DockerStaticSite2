9a394a549909e67f61345808d8ef42d240118925
hwarang098tkd <hwarang098tkd@gmail.com>
2024-12-30 20:25:04 +0200

    scrippt

--- matomo.js
+++ matomo.js
@@ -2,10 +2,10 @@ var _paq = window._paq = window._paq || [];
 /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
-(function() {
-    var u="https://davidvdanos.ddns.net/";
-    _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '2']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+(function () {
+    var u = "//daviddevelopment.ddns.net/";
+    _paq.push(['setTrackerUrl', u + 'eliotracker.php']);
+    _paq.push(['setSiteId', '3']);
+    var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+    g.async = true; g.src = u + 'eliotracker.js'; s.parentNode.insertBefore(g, s);
 })();
