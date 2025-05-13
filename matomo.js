c7377ad9ed22fd936df1d1c70972ecf7459271af
David-Vasileios Ntanos <hwarang098tkd@gmail.com>
2025-05-13 10:53:22 +0300

    Update matomo.js

--- matomo.js
+++ matomo.js
@@ -1,11 +1,12 @@
-var _paq = window._paq = window._paq || [];
-/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-_paq.push(['trackPageView']);
-_paq.push(['enableLinkTracking']);
-(function() {
-var u="https://elioplustrackingdev.com/";
-_paq.push(['setTrackerUrl', u+'eliotracker.php']);
-_paq.push(['setSiteId', '27']);
-var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-g.async=true; g.src=u+'eliotracker.js'; s.parentNode.insertBefore(g,s);
-})();
+
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="https://elioplustrackingdev.com/";
+    _paq.push(['setTrackerUrl', u+'eliotracker.php']);
+    _paq.push(['setSiteId', '29']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src=u+'eliotracker.js'; s.parentNode.insertBefore(g,s);
+  })();
